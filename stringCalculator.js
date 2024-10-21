c1d2d25a5e86425df6ce101ceb739b62c0d82625
JitendraSingh <js362676@gmail.com>
2024-10-21 22:15:43 +0530

    single number returns the number two numbers return their sum

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,8 +1,10 @@
 function add(numbers) {
     if (numbers === "") {
         return 0;
+    } else if (numbers.includes(",")) {
+        return numbers.split(",").reduce((sum, num) => sum + parseInt(num), 0);
     }
-    return 0;
+    return parseInt(numbers);
 }
 
 module.exports = { add };
--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -3,3 +3,11 @@ const { add } = require('./stringCalculator');
 test('empty string returns 0', () => {
     expect(add("")).toBe(0);
 });
+
+test('single number returns the number', () => {
+    expect(add("1")).toBe(1);
+});
+
+test('two numbers return their sum', () => {
+    expect(add("1,5")).toBe(6);
+});
